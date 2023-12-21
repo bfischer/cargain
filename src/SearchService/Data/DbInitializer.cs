fd44d3602cc32d4d254b331136f3fabfecc6f3ce
Blake Fischer <blake.fischer@dsa019922.docusignhq.com>
2023-12-21 15:37:50 -0800

    added support for sync communication between services, added resiliency to search service startup

---
+++ src/SearchService/Data/DbInitializer.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using MongoDB.Entities;
+
+namespace SearchService;
+
+public class DbInitializer
+{
+    public static async Task InitDb(WebApplication app)
+    {
+        await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+
+        await DB.Index<Item>()
+            .Key(i => i.Make, KeyType.Text)
+            .Key(i => i.Model, KeyType.Text)
+            .Key(i => i.Color, KeyType.Text)
+            .CreateAsync();
+
+        var count = await DB.CountAsync<Item>();
+
+        using var scope = app.Services.CreateScope();
+
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+
+        var items = await httpClient.GetItemsForSearchDb();
+
+        Console.WriteLine($"{items.Count} items returned from Auction service.");
+
+        if (items.Count > 0) await DB.SaveAsync(items);
+    }
+}
